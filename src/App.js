032bfdf3e4dbeb5ab4d466eabbc975df86929870
Sarvesh Nagar <sarveshnagar08@gmail.com>
2021-12-27 01:44:43 +0530

    3 grids added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,177 @@
-import logo from './logo.svg';
+import React, {useEffect, useState, useRef} from 'react';
+import axios from 'axios';
+import { Header } from './components/Header';
+import { 
+  MDBInputGroup, 
+  MDBInputGroupElement, 
+  MDBBtn, 
+  MDBIcon,
+  MDBCard, 
+  MDBCardBody, 
+  MDBCardTitle, 
+  MDBCardText,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBTable,
+  MDBTableBody } from 'mdb-react-ui-kit';
 import './App.css';
 
 function App() {
+  // useEffect(() => {
+  //   axios.get('https://api.openweathermap.org/data/2.5/weather?q=Indore&appid=c3f0a51be707c69778e40244469ebb16')
+  //     .then(res => {
+  //       console.log(res)
+  //     })
+  //     .catch(err => {
+  //       console.log(err)
+  //     })
+  // })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <Search></Search>
+    </>
   );
 }
+
+function Search() {
+  const [formValue, setFormValue] = useState('');
+  const [city, setCity] = useState([]);
+  const [atm, setAtm] = useState([]);
+  const [wind, setWind] = useState([]);
+  const [flag, setFlag] = useState(1);
+  const [name, setName] = useState('');
+  const [sys, setSys] = useState([]);
+  const [cor, setCor] = useState([]);
+
+  const search = e => {
+    e.preventDefault()
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q='+ formValue +'&units=metric&appid=c3f0a51be707c69778e40244469ebb16')
+      .then(res => {
+        console.log(res)
+        console.log(res.data.main)
+        setCity(res.data.main)
+        setAtm(res.data.weather[0])
+        setWind(res.data.wind)
+        setName(res.data.name)
+        setSys(res.data.sys)
+        setCor(res.data.coord)
+        setFlag(1);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      setFormValue('');
+    }
+  
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-center font" style={{minHeight: '50vh'}}>
+        <MDBCard className='z-depth-5 m-3 font' style={{ maxWidth: '50rem'}}>
+          <MDBCardBody>
+            {/* <MDBCardTitle>Card title</MDBCardTitle> */}
+            {/* <MDBCardText>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </MDBCardText> */}
+            <form onSubmit={search}>
+              <MDBInputGroup onSubmit={search} className='mb-3' size='lg'>
+                <MDBInputGroupElement value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Enter City Name" type='text' />
+                <MDBBtn type="submit" disabled={!formValue} style={{backgroundColor: '#2E2E2E'}}><MDBIcon fas icon='search' /></MDBBtn>
+              </MDBInputGroup>
+            </form>
+            {/* <MDBBtn>Button</MDBBtn> */}
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      { flag ? <Details temp = {city.temp} atm = {atm.description} hum = {city.humidity}
+        name = {name} wind = {wind.speed} sea = {city.sea_level} sunrise = {sys.sunrise} 
+        sunset = {sys.sunset} country = {sys.country} lat = {cor.lat} lon = {cor.lon}></Details> : ""}
+      
+    </>
+  );
+}
+
+function Details(props) {
+  return (
+    <>
+      <div className='font' style={{textAlign: 'center'}}>
+        <h4>{props.name}, {props.country}</h4>
+      </div>
+      <div className='font'>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol size='md' className='col-example'>
+              <MDBCard style={{ maxWidth: '22rem' }}>
+                <MDBCardBody>
+                  {/* <MDBCardTitle>Card title</MDBCardTitle> */}
+                    <MDBTable>
+                      <MDBTableBody>
+                        <tr>
+                          <th scope='row'>Temperature</th>
+                          <td>{props.temp}°C</td>
+                        </tr>
+                        <tr>
+                          <th scope='row'>Atmosphere</th>
+                          <td>{props.atm}</td>
+                        </tr>
+                        <tr>
+                          <th scope='row'>Humidity</th>
+                          <td>{props.hum}%</td>
+                        </tr>
+                      </MDBTableBody>
+                    </MDBTable>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol size='md' className='col-example'>
+              <MDBCard style={{ maxWidth: '22rem' }}>
+                <MDBCardBody>
+                  {/* <MDBCardTitle>Card title</MDBCardTitle> */}
+                  <MDBTable>
+                      <MDBTableBody>
+                        <tr>
+                          <th scope='row'>Wind</th>
+                          <td>{(props.wind * 3.6).toFixed(2)} kmph</td>
+                        </tr>
+                        <tr>
+                          <th scope='row'>Sea Level</th>
+                          <td>{props.sea} hPA</td>
+                        </tr>
+                        <tr>
+                          <th scope='row'>Coordinates</th>
+                          <td>{props.lat}°  {props.lon}° </td>
+                        </tr>
+                      </MDBTableBody>
+                    </MDBTable>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol size='md' className='col-example'>
+              <MDBCard style={{ maxWidth: '22rem' }}>
+                <MDBCardBody>
+                  {/* <MDBCardTitle>Card title</MDBCardTitle> */}
+                  <MDBTable>
+                    <MDBTableBody>
+                      <tr>
+                        <th scope='row'>Sunrise</th>
+                        <td>{props.sunrise}</td>
+                      </tr>
+                      <tr>
+                        <th scope='row'>Sunset</th>
+                        <td>{props.sunset}</td>
+                      </tr>
+                    </MDBTableBody>
+                  </MDBTable>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
+    </>
+  )
+}
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBDropdownLink,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
+
+export const Header = () => {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <MDBNavbar className='font shadow-0' expand='lg' light bgColor='white'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'> <b>cloudy.</b> </MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowBasic(!showBasic)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showBasic}>
+          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0' style={{textAlign: 'center'}}>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>About</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
